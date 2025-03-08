7f2cb2b5663aaeda2dddb4fe38cd33fd95dcb74f
Aaron3312 <a01642529@tec.mx>
2025-03-07 19:24:58 -0600

    cositasCondig

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "cypress";
 
+
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173", // URL por defecto de Vite
+    baseUrl: 'http://localhost:5173',
+    defaultCommandTimeout: 10000, // Tiempo de espera predeterminado para la mayoría de comandos
+    pageLoadTimeout: 30000,      // Tiempo de espera para cargar la página
+    requestTimeout: 15000,       // Tiempo de espera para peticiones XHR/AJAX
+    responseTimeout: 30000,      // Tiempo de espera para respuestas
     experimentalStudio: true,
     setupNodeEvents(on, config) {
       // implementar event listeners de Node.js aquí
